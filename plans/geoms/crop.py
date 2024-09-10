f56eaab0f4ed2bb992cc8503098977701d8060bb
Ekaterina Arkhipova <horis.earkhipova@gmail.com>
2024-09-10 18:15:48 +0200

    Restart notebooks.

--- plans/geoms/crop.py
+++ plans/geoms/crop.py
@@ -6,7 +6,7 @@ from PIL import Image, ImageDraw
 import shapely
 import numpy as np
 
-from lets_plot import geom_image
+from lets_plot import geom_imshow
 
 def get_geom_mask(geom, width, height):
     polygon = list(geom.exterior.coords)
@@ -14,7 +14,7 @@ def get_geom_mask(geom, width, height):
     ImageDraw.Draw(im_mask).polygon(polygon, outline=1, fill=1)
     return im_mask
 
-def get_geom_image(geom, image):
+def get_geom_imshow(geom, image):
     origin = (int(geom.bounds[0]), int(geom.bounds[1]))
     total_width, total_height = int(geom.bounds[2]), int(geom.bounds[3])
     width, height = total_width - origin[0], total_height - origin[1]
@@ -35,13 +35,13 @@ def get_geom_image(geom, image):
     return image_rgba
 
 def get_stub_image(geometry):
-    return geom_image(image_data=np.array([[[0, 0, 0, 0]]]))
+    return geom_imshow(image_data=np.array([[[0, 0, 0, 0]]]))
 
 def get_geom_crop(geometry, image_url):
     if image_url:
         try:
-            image_rgba = get_geom_image(geometry, Image.open(BytesIO(requests.get(image_url).content)))
-            return geom_image(image_data=np.asarray(image_rgba))
+            image_rgba = get_geom_imshow(geometry, Image.open(BytesIO(requests.get(image_url).content)))
+            return geom_imshow(image_data=np.asarray(image_rgba))
         except:
             return get_stub_image(geometry)
     else:
